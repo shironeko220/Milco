c3ad11a3692fbfba44b36481741edac6d463be76
Chitose <shironeko220@qq.com>
2023-05-17 13:31:01 +0800

    qrcode

---
+++ .history/site.config_20230517121246.ts
@@ -0,0 +1,125 @@
+import { defineSiteConfig } from 'valaxy'
+
+export default defineSiteConfig({
+  url: 'https://milco.fun/',
+  lang: 'zh-CN',
+  subtitle: '',
+  title: '沐可的小站',
+  author: {
+    avatar: 'https://q1.qlogo.cn/g?b=qq&nk=1531169227&s=640',
+    name: '沐可',
+    status: {
+      emoji: '🎀'
+    }
+  },
+  description: '爱如雨下，恋如雨止，思如星远，念如岁长',
+  social: [
+    {
+      name: 'RSS',
+      link: '/atom.xml',
+      icon: 'i-ri-rss-line',
+      color: 'orange',
+    },
+    {
+      name: 'QQ 1531169227',
+      link: 'tencent://message/?uin=1531169227',
+      icon: 'i-ri-qq-line',
+      color: '#12B7F5',
+    },
+    {
+      name: 'GitHub',
+      link: 'https://github.com/shironeko220',
+      icon: 'i-ri-github-line',
+      color: '#6e5494',
+    },
+    // {
+    //   name: '微博',
+    //   link: '',
+    //   icon: 'i-ri-weibo-line',
+    //   color: '#E6162D',
+    // },
+    // {
+    //   name: '豆瓣',
+    //   link: '',
+    //   icon: 'i-ri-douban-line',
+    //   color: '#007722',
+    // },
+    {
+      name: '网易云音乐',
+      link: 'https://music.163.com/#/user/home?id=586801557',
+      icon: 'i-ri-netease-cloud-music-line',
+      color: '#C20C0C',
+    },
+    {
+      name: '知乎',
+      link: 'https://www.zhihu.com/people/meng-yue-luo-ya',
+      icon: 'i-ri-zhihu-line',
+      color: '#0084FF',
+    },
+    {
+      name: '哔哩哔哩',
+      link: 'https://space.bilibili.com/74924105',
+      icon: 'i-ri-bilibili-line',
+      color: '#FF8EB3',
+    },
+    // {
+    //   name: '微信公众号',
+    //   link: '',
+    //   icon: 'i-ri-wechat-2-line',
+    //   color: '#1AAD19',
+    // },
+    // {
+    //   name: 'Twitter',
+    //   link: 'https://twitter.com/YunYouJun',
+    //   icon: 'i-ri-twitter-line',
+    //   color: '#1da1f2',
+    // },
+    // {
+    //   name: 'Telegram Channel',
+    //   link: 'https://t.me/elpsycn',
+    //   icon: 'i-ri-telegram-line',
+    //   color: '#0088CC',
+    // },
+    {
+      name: 'E-Mail',
+      link: 'mailto:Milco220@126.com',
+      icon: 'i-ri-mail-line',
+      color: '#8E71C1',
+    },
+    // {
+    //   name: 'Travelling',
+    //   link: 'https://www.travellings.cn/go.html',
+    //   icon: 'i-ri-train-line',
+    //   color: 'var(--va-c-text)',
+    // },
+  ],
+
+  search: {
+    enable: false,
+  },
+
+  sponsor: {
+    enable: true,
+    title: '我很可爱，请给我钱！',
+    methods: [
+      {
+        name: '支付宝',
+        url: 'https://r2.milco.fun/alipay-qrcode.jpg',
+        color: '#00A3EE',
+        icon: 'i-ri-alipay-line',
+      },
+      {
+        name: 'QQ 支付',
+        url: 'https://r2.milco.fun/qqpay-qrcode.png',
+        color: '#12B7F5',
+        icon: 'i-ri-qq-line',
+      },
+      {
+        name: '微信支付',
+        url: 'https://r2.milco.fun/wechatpay-qrcode.png',
+        color: '#2DC100',
+        icon: 'i-ri-wechat-pay-line',
+      },
+    ],
+  },
+})
